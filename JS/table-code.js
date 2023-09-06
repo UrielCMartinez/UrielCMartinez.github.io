b254f27bc359d3164f964a8b8649266e7f88353e
UrielCMartinez <urielc.developer@gmail.com>
2023-09-06 14:13:14 -0600

    AquaApp de ingles
    Se subio la app de ingles

---
+++ JS/table-code.js
@@ -0,0 +1,24 @@
+$(function(){
+    var table;
+    $.getJSON('JSon/verbos.json',function(data){
+        table=data.table;
+        iniciando(0);
+    });
+    function iniciando(){
+        $("table-verbs").empty();
+        jQuery.each(table.rows, function(i,row){
+            var r="";
+            $.each(row,function(index,valor){
+                if(index=="Infinitive"){
+                    r=r+"<tr><td>"+"<span>"+valor+"</td>";
+                }
+                else{
+                    r=r+"<td>"+valor+"</td>";
+                }
+                
+            });
+            r+="</tr>";
+            $("table").append(r);
+        });
+    }
+});
